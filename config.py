9b180b0f962c156b8a173ca9b56b8c342592ccaa
vpn7318 <himanshusingh7318021404@gmail.com>
2024-09-04 11:48:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7325380060:AAG_y0cJgCr8uOaglaayyU_H_Vmy9-EkTI8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("itsvpn1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1783623793")
